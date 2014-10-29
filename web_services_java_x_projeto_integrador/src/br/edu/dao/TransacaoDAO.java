774746a662f3e71f6f29156c1b1a9945050d5b72
ricardo <ricardo@localhost.localdomain>
2014-10-29 01:11:10 -0200

    Incluído "banco de dados" em xml como determina o enunciado.

--- web_services_java_x_projeto_integrador/src/br/edu/dao/TransacaoDAO.java
+++ web_services_java_x_projeto_integrador/src/br/edu/dao/TransacaoDAO.java
@@ -1,25 +1,96 @@
 package br.edu.dao;
 
 import br.edu.modelo.Transacao;
+import java.io.File;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.xml.sax.SAXException;
 
 public class TransacaoDAO {
     
-    private List<Transacao> transacoes;
+    private Document bancoDados;
+    private File arquivoBancoDados;
     
-    public TransacaoDAO() {
-        transacoes = new ArrayList<>();
-    }            
-    
-    public List<Transacao> get() {
-        return transacoes;
+    public TransacaoDAO() 
+            throws ParserConfigurationException, SAXException, IOException {
+        arquivoBancoDados = new File("banco_dados.xml");
+
+        if (arquivoBancoDados.exists()) 
+            bancoDados = carregarArquivo(arquivoBancoDados);
+        else
+            bancoDados = criarArquivo();
     }
     
-    public List<Transacao> get(Long numeroConta) {
+    private Document criarArquivo() throws ParserConfigurationException {
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        Document bd = dBuilder.newDocument();        
+        
+        Element transacoes = bd.createElement("transacoes");
+        
+        bd.appendChild(transacoes);
+        
+        return bd;
+    }            
+    
+    private Document carregarArquivo(File arquivoXml) 
+            throws ParserConfigurationException, SAXException, IOException {
+        
+        DocumentBuilderFactory fabrica = DocumentBuilderFactory.newInstance();        
+        DocumentBuilder construtor = fabrica.newDocumentBuilder();
+        return construtor.parse(arquivoXml);
+    }
+    
+    public List<Transacao> get() throws ParseException {
+        Node transacoes = bancoDados.getElementsByTagName("transacoes").item(0);
+        List<Transacao> lista = new ArrayList<>();
+        
+        for (int i = 0; i < transacoes.getChildNodes().getLength(); i++) {
+            Node noTransacao = transacoes.getChildNodes().item(i);
+            Transacao transacao = new Transacao();
+            
+            for (int j = 0; j < noTransacao.getChildNodes().getLength(); j++) {
+                Node no = noTransacao.getChildNodes().item(j);
+                
+                if      (no.getNodeName().equalsIgnoreCase("conta"))
+                    transacao.setConta(Long.parseLong(no.getTextContent()));
+                else if (no.getNodeName().equalsIgnoreCase("tipo_transacao"))
+                    transacao.setTipoTransacao(no.getTextContent());
+                else if (no.getNodeName().equalsIgnoreCase("descricao"))
+                    transacao.setDescricao(no.getTextContent());
+                else if (no.getNodeName().equalsIgnoreCase("data")) {
+                    SimpleDateFormat format = 
+                                            new SimpleDateFormat("dd/MM/yyyy");  
+                    transacao.setData(format.parse(no.getTextContent()));
+                }                    
+            }
+            
+            lista.add(transacao);
+        }
+        
+        return lista;
+    }
+    
+    public List<Transacao> get(Long numeroConta) throws ParseException {
+        List<Transacao> todas = get();
         List<Transacao> retornos = new ArrayList<>();
         
-        for (Transacao transacao : transacoes) {
+        for (Transacao transacao : todas) {
             if (transacao.getConta().equals(numeroConta))
                 retornos.add(transacao);
         }
@@ -30,7 +101,38 @@ public class TransacaoDAO {
             return retornos;
     }
     
-    public void salvar(Transacao transacao) {
-        this.transacoes.add(transacao);
+    public void salvar(Transacao transacao) throws TransformerException {        
+        Node transacoes = bancoDados.getElementsByTagName("transacoes").item(0);
+        Element trans = bancoDados.createElement("transacao");    
+        
+        transacoes.appendChild(trans);
+        
+        Element conta = bancoDados.createElement("conta");
+        conta.setTextContent(transacao.getConta().toString());        
+        trans.appendChild(conta);
+        
+        Element tipo = bancoDados.createElement("tipo_transacao");
+        tipo.setTextContent(transacao.getTipoTransacao());        
+        trans.appendChild(tipo);
+        
+        Element descricao = bancoDados.createElement("descricao");
+        descricao.setTextContent(transacao.getDescricao());        
+        trans.appendChild(descricao);
+
+        Element data = bancoDados.createElement("data");
+        SimpleDateFormat formatador = new SimpleDateFormat("dd/MM/yyyy");          
+        
+        data.setTextContent(formatador.format(transacao.getData()));        
+        trans.appendChild(data);
+        
+        salvarBancoDados();
+    }
+    
+    private void salvarBancoDados() throws TransformerException {
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource domsource = new DOMSource(bancoDados);
+        StreamResult streamResult = new StreamResult(arquivoBancoDados);        
+        transformer.transform(domsource, streamResult);        
     }
 }
--- web_services_java_x_projeto_integrador/src/br/edu/servico/TransacaoResource.java
+++ web_services_java_x_projeto_integrador/src/br/edu/servico/TransacaoResource.java
@@ -2,6 +2,8 @@ package br.edu.servico;
 
 import br.edu.dao.TransacaoDAO;
 import br.edu.modelo.Transacao;
+import java.io.IOException;
+import java.text.ParseException;
 import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -10,32 +12,51 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import org.xml.sax.SAXException;
 
 @Path("transacao")
 public class TransacaoResource {
     
     // Ao criar inicia a lista de transacoes pela primeira vez
-    private static TransacaoDAO dao = new TransacaoDAO();
-    
     @GET
     @Produces(MediaType.APPLICATION_XML)
     public List<Transacao> getTransacoes() {
-        return dao.get();
+        try {
+            TransacaoDAO dao = new TransacaoDAO();
+            return dao.get();
+        } catch (ParseException | ParserConfigurationException |
+                 SAXException | IOException ex) {
+            return null;
+        }
     }    
     
     @Path("{numero_conta}")
     @GET
     @Produces(MediaType.APPLICATION_XML)
     public List<Transacao> get(@PathParam("numero_conta") long numeroConta) {        
-        return dao.get(numeroConta);
+        try {
+            TransacaoDAO dao = new TransacaoDAO();
+            return dao.get(numeroConta);
+        } catch (ParseException | ParserConfigurationException |
+                 SAXException | IOException ex) {
+            return null;
+        }
     }
     
     @POST
     @Consumes(MediaType.APPLICATION_XML)
     @Produces(MediaType.APPLICATION_XML)
     public Transacao adicionarTransacao(Transacao transacao) {        
-        dao.salvar(transacao);
-        return transacao;
+        try {
+            TransacaoDAO dao = new TransacaoDAO();
+            dao.salvar(transacao);            
+            return transacao;
+        } catch (ParserConfigurationException | SAXException |
+                 IOException | TransformerException ex) {
+            return null;
+        }
     }
     
     /**
